3abb18477c5b183e24967a1354f1f2b262077a0f
Henry Mao <admin@calclavia.com>
2017-02-16 11:21:40 -0800

    Added colors

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -46,4 +46,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
     console.log("User clicked on color button");
+    var url = '/palette';
+
+    $.get(url)
+        .then(function(res) {
+            var colors = res.colors.hex;
+            $('body').css('background-color', colors[0]);
+            $('.thumbnail').css('background-color', colors[1]);
+            $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+            $('p').css('color', colors[3]);
+            $('.project img').css('opacity', .75);
+        });
 }
