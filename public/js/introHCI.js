fc29d96f1d849e00c2d802520bb7249808c9f09b
Henry Mao <admin@calclavia.com>
2017-02-16 11:17:06 -0800

    Added project details

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,31 +2,42 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+    $('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+    $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+    // Prevent following the link
+    e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+    // Get the div ID, e.g., "project3"
+    var projectID = $(this).closest('.project').attr('id');
+    // get rid of 'project' from the front of the id 'project3'
+    var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+    console.log("User clicked on project " + idNumber);
+
+    var url = '/project/' + idNumber;
+
+    $.get(url)
+        .then(function(res) {
+            // Build HTML
+            var html = '';
+            html += '<img class="detailsImage" src="' + res.image + '"/>';
+            html += '<p>' + res.summary + '</p>';
+            $('#project' + idNumber + ' .details').html(html);
+        });
 }
 
 /*
@@ -34,5 +45,5 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+    console.log("User clicked on color button");
 }
